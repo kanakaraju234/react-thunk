44cd891c9771553ba92fc8e9e6f4ed9649571096
projects <gorlejanak0@gmail.com>
2024-04-14 12:03:32 +0530

    signup changes updated

---
+++ src/bookings/BookingRequest.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import './BookingRequest.css';
+
+const BookinRequest = () => {
+  return (
+    <div className='row request-data  container-fluid overflow-hidden ' style={{height:'100%',width:'100%'}} >
+    <div className='shadow col-10 rounded  m-2 w-100  bg-light d-flex justify-content-between overflow-hidden'>
+    <div className=''>
+       <h1>Master Booking</h1>
+       <p>please select booking to see requests</p>
+    </div>
+    <div>
+    <button className='bi bi-plus-circle float-end  mt-5  p-2 text-light rounded border border-0'
+     style={{backgroundColor:'blue',}}>&nbsp;Create Master Booking</button>
+    </div>
+    </div>
+{/* second-one */}
+<div className='m-2 border-0 rounded bg-light overflow-hidden' style={{height:'100vh'}}>
+<div className='mt-3 d-flex justify-content-between'>
+   <div className='input-group mt-4 '>
+    <button className='bi bi-search btn border border-1'></button>
+ <input type="text" className='form-control me-2 '  style={{width:'600px',height:'50px'}} />
+   </div>
+  <span>Page Size <input type="text" className='form-control me-3 p-1 ' placeholder='10'
+ style={{width:'100px',height:'50px'}} /></span>
+<input type="text" className=' bi bi-0-circle form-control mt-4 p-2'  value='filter' style={{width:'70px',height:'50px'}} />
+  </div>
+
+{/* //3rd table */}
+<div className='mt-4 mx-auto' >
+  <table className='table table-hover'>
+<thead>
+  <tr>
+    <th>Booking Reference</th>
+    <th>Created Data</th>
+    <th>Status</th>
+    <th>Actions</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>MOJB05033</td>
+    <td>01-4-2023</td>
+    <td><button className='btn btn-outline-primary '>Active</button></td>
+    <td><button className='btn btn-close'></button></td>
+  </tr>
+</tbody>
+<tbody>
+  <tr>
+    <td>MOJB05034</td>
+    <td>02-4-2023</td>
+    <td><button className='btn btn-outline-primary '>Active</button></td>
+    <td><button className='btn btn-close'></button></td>
+  </tr>
+</tbody>
+<tbody>
+  <tr>
+    <td>MOJB05033</td>
+    <td>03-4-2023</td>
+    <td><button className='btn btn-outline-success '>Active</button></td>
+    <td><button className='btn btn-close'></button></td>
+  </tr>
+</tbody>
+<tbody>
+  <tr>
+    <td>MOJB05034</td>
+    <td>04-4-2023</td>
+    <td><button className='btn btn-outline-success '>Active</button></td>
+    <td><button className='btn btn-close'></button></td>
+  </tr>
+</tbody>
+<tbody>
+  <tr>
+    <td>MOJB05030</td>
+    <td>05-4-2023</td>
+    <td><button className='btn btn-outline-success '>Active</button></td>
+    <td><button className='btn btn-close'></button></td>
+  </tr>
+</tbody>
+<tbody>
+  <tr>
+    <td>MOJB05038</td>
+    <td>06-4-2023</td>
+    <td><button className='btn btn-outline-primary '>Active</button></td>
+    <td><button className='btn btn-close'></button></td>
+  </tr>
+</tbody>
+<tbody>
+  <tr>
+    <td>MOJB05037</td>
+    <td>07-4-2023</td>
+    <td><button className='btn btn-outline-primary '>Active</button></td>
+    <td><button className='btn btn-close'></button></td>
+  </tr>
+</tbody>
+<tbody>
+  <tr>
+    <td>MOJB05036</td>
+    <td>07-4-2023</td>
+    <td><button className='btn btn-outline-primary '>Active</button></td>
+    <td><button className='btn btn-close'></button></td>
+  </tr>
+</tbody>
+  </table>
+</div>
+
+</div>
+
+    </div>
+  )
+}
+
+export default BookinRequest
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { signUp } from "../redux/Reducers/UserReducer";
 import { useNavigate } from "react-router-dom";
-
 export const Signup = () => {
   const [user, setUser] = useState({
     name: "",
@@ -11,40 +10,80 @@ export const Signup = () => {
     password: "",
     confirmPassword: "",
   });
+  const [errors,setErrors] = useState({});
+
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   console.log("user", user);
-  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { confirmPassword, ...rest } = user;
-    dispatch(signUp({ ...rest, created_at: new Date(), token: "newtoken" }));
-       setUser({
-        name: "",
-        phoneNumber: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      });
-       navigate("/login")
-    
+// validating have to do here 
+
+                   const validationErrors = {};
+           if (!user.name.trim()) {
+  validationErrors.name = "Name is required";
+}
+if (!user.phoneNumber.trim()) {
+  validationErrors.phoneNumber = "Phone number is required";
+} else if (!/^\d{10}$/.test(user.phoneNumber)) {
+          validationErrors.phoneNumber = "Invalid phone number";
+}
+if (!user.email.trim()) {
+            validationErrors.email = "Email is required";
+           }else if (!/\S+@\S+\.\S+/.test(user.email)) {
+  validationErrors.email = "Invalid email address";
+}
+    if (!user.password.trim()) {
+      validationErrors.password = "Password is required";
+        } else if (user.password.length < 6) {
+  validationErrors.password = "Password must be at least 6 characters or  above";
+}
+// cpassword  && password should be same
+            if (user.confirmPassword !== user.password) {
+            validationErrors.confirmPassword = "Password do not match";
+}
+
+// if everything is right 
+if (Object.keys(validationErrors).length === 0) {
+  const { confirmPassword, ...rest } = user;
+  dispatch(signUp({ ...rest, created_at: new Date(), token: "newtoken" }));
+  setUser({
+    name: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  alert('Form submitted successfully!');
+  navigate("/login");
+} else {
+  setErrors(validationErrors);
+}
+
+// first i have to try 
+
   };
+// validation  upto here
+
+// up to here janu
 
   const handleChange = (e) => {
     setUser({
       ...user,
       [e.target.name]: e.target.value,
     });
-    console.log("onchange" , user)
   };
 
   return (
-    <div className="d-flex bg-white align-items-center justify-content-center  container rounded 0 ">
+    <div className="d-flex bg-white align-items-center justify-content-center container rounded 0 h-25 ">
       <form
         className="form-control form-outline border-0"
         onSubmit={handleSubmit}
       >
+        <h1 className="text-center">Signup</h1>
         <div className="form-group w-100 mb-2 mt-4">
+          {/* name */}
           <label htmlFor="name">Name</label>
           <input
             type="text"
@@ -54,6 +93,8 @@ export const Signup = () => {
             placeholder="Enter Name"
             onChange={(e) => handleChange(e)}
           />
+           {errors.name && <div className="text-danger">{errors.name}</div>}
+           {/* num */}
           <label htmlFor="phone">Phone Number</label>
           <input
             type="text"
@@ -63,6 +104,8 @@ export const Signup = () => {
             placeholder="Enter Phone Number"
             onChange={(e) => handleChange(e)}
           />
+            {errors.phoneNumber && <div className="text-danger">{errors.phoneNumber}</div>}
+            {/* email */}
           <label htmlFor="emial address">Emaild address</label>
           <input
             type="email"
@@ -72,7 +115,8 @@ export const Signup = () => {
             placeholder="Enter Email"
             onChange={(e) => handleChange(e)}
           />
-
+   {errors.email && <div className="text-danger">{errors.email}</div>}
+    {/* // password */}
           <label htmlFor="password">Password</label>
           <input
             type="password"
@@ -82,7 +126,8 @@ export const Signup = () => {
             placeholder="Password"
             onChange={(e) => handleChange(e)}
           />
-
+ {errors.password && <div className="text-danger">{errors.password}</div>}
+ {/* here cPassword */}
           <label htmlFor="confirm password" className="mt-2">
             Confirm Password
           </label>
@@ -94,8 +139,8 @@ export const Signup = () => {
             className="form-control mb-3 mt-2"
             onChange={(e) => handleChange(e)}
           />
-
-          <button type="submit" className="btn btn-primary mb-3">
+ {errors.confirmPassword && <div className="text-danger">{errors.confirmPassword}</div>}
+          <button type="submit" className="btn btn-primary  mt-3 w-100 mb-3">
             Submit
           </button>
         </div>
